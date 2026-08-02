62633a73ca77a329b419f516ad469030ab566278
pixelweld <310593137+pixelweld@users.noreply.github.com>
2026-08-02 16:21:30 +0300

    feat: add system information command

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-print("=== PyGuard ===")
-print('1. Информация о системе')
-print('0. Выход')
+import platform
 
-#ввод
-inp = input('> ')
+def show_system_info():
+    print('Операционная система:', platform.system())
+    print('Версия ядра:', platform.release())
+    print('Архитектура:', platform.machine())
+    print('Имя компьютера:', platform.node())
 
-if inp == '1':
-    print('Информация о вашей системе: ')
-elif inp == '0':
-    print('Вы решили выйти' )
-else:
-    print('Неизвестная команда')
+while True:
+
+    print("=== PyGuard ===")
+    print('1. Информация о системе')
+    print('0. Выход')
+
+    #ввод
+    inp = input('> ')
+
+
+    if inp == '1':
+        print('===')
+        show_system_info()
+        print('===')
+        input('Нажмите Enter, чтобы вернуться в меню...')
+        
+    elif inp == '0':
+        print('Вы решили выйти')
+        break
+    else:
+        print('Неизвестная команда')
