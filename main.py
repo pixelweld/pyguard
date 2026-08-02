9e8ed77c240bc9b933dd70f9ba2cce4cfd58d570
pixelweld <310593137+pixelweld@users.noreply.github.com>
2026-08-02 16:54:18 +0300

    feat: add disk information command

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import platform
+import shutil
+
+def show_disk_info():
+    shutil.disk_usage('/')
+    disk = shutil.disk_usage('/')
+    
+    total_gb = disk.total / (1024 ** 3)
+    print('Всего места:', round(total_gb, 2), 'ГБ')
+    
+    used_gb = disk.used / (1024 ** 3)
+    print('Заполнено:', round(used_gb, 2), 'ГБ')
+    
+    free_gb = disk.free / (1024 ** 3)
+    print('Свободно:', round(free_gb, 2), 'ГБ')
 
 def show_system_info():
     print('Операционная система:', platform.system())
@@ -6,22 +20,31 @@ def show_system_info():
     print('Архитектура:', platform.machine())
     print('Имя компьютера:', platform.node())
 
+def wait_for_enter():
+    input('Нажмите Enter, чтобы вернуться в меню...')
+
+def skobki_tu():
+    print('===')
+
 while True:
 
     print("=== PyGuard ===")
+    print('2. Информация о диске')
     print('1. Информация о системе')
     print('0. Выход')
-
     #ввод
-    inp = input('> ')
-
+    inp = input('>> ')
 
     if inp == '1':
-        print('===')
+        skobki_tu()
         show_system_info()
-        print('===')
-        input('Нажмите Enter, чтобы вернуться в меню...')
-        
+        skobki_tu()
+        wait_for_enter()
+    elif inp == '2':
+        skobki_tu()
+        show_disk_info() 
+        skobki_tu() 
+        wait_for_enter()
     elif inp == '0':
         print('Вы решили выйти')
         break
